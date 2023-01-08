4b92a482fec6fa31464e8f239affdd1678abdf56
kelvin <kelvinvaleti@gmail.com>
2023-01-08 16:13:54 -0300

    funcao gerarTabelaEscalacao

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
-let escalacao = [];
+/**
+ * Variaveis
+ */
+
+//a escalação já inicia com alguns nomes.
+let escalacao = [
+    new Jogador(1, "Allisson", "Goleiro"),
+    new Jogador(4, "Marquinhos", "Zagueiro"),
+    new Jogador(3, "T. Silva", "Zagueiro"),
+    new Jogador(2, "Danilo", "Lateral"),
+    new Jogador(14, "É. Militão", "Meia"),
+    new Jogador(20, "Vinicius Jr", "Atacante")
+];
+
+//construtor objeto
+function Jogador(camisaJogador, nomeJogador, posicaoJogador){
+    this.camisa = camisaJogador;
+    this.nome= nomeJogador;
+    this.posicao = posicaoJogador;
+}
 
 /**
  * FUNÇÕES
  */
-function preencherEscalacaoModelo(){
-    console.log("entrou");
+function gerarTabelaEscalacao(){
+    
+    for(let i=0;i<escalacao.length;i++){
+        const table = document.getElementById("table");
+        const tableTr = document.createElement("tr");
+        table.appendChild(tableTr);
+
+        const tdCamisa = document.createElement("td");
+        tdCamisa.innerText=escalacao[i].camisa;
+        tableTr.appendChild(tdCamisa);
+
+        const tdNomeJogador = document.createElement("td");
+        tdNomeJogador.innerText=escalacao[i].nome;
+        tableTr.appendChild(tdNomeJogador);
+
+        const tdPosicao = document.createElement("td");
+        tdPosicao.innerText=escalacao[i].posicao;
+        tableTr.appendChild(tdPosicao);
+
+        const editarIcon = document.createElement("td");
+        const iTagEdit = document.createElement("i");
+        iTagEdit.classList.add("fa");
+        iTagEdit.classList.add("fa-pencil");
+        editarIcon.appendChild(iTagEdit);
+        tableTr.appendChild(editarIcon);
+
+        const deleteIcon = document.createElement("td");
+        const iTagDelete = document.createElement("i");
+        iTagDelete.classList.add("fa");
+        iTagDelete.classList.add("fa-trash");
+        deleteIcon.appendChild(iTagDelete);
+        tableTr.appendChild(deleteIcon);
+    }
 }
 
 
@@ -14,4 +64,4 @@ function preencherEscalacaoModelo(){
  * EVENTOS
  */
 
-window.onload = preencherEscalacaoModelo();
+window.onload = gerarTabelaEscalacao();
