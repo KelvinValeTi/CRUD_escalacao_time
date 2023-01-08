5d79a5b41cca0fac8c23daa4fb001f2921969ff1
kelvin <kelvinvaleti@gmail.com>
2023-01-08 18:31:03 -0300

    funcao escalar jogador

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
  * Variaveis
  */
 
+//botões
 const btnApagarTodos = document.getElementById("apagar-todos");
+const btnAlterarNomeTime = document.getElementById("btn-alterar-nome-time");
+const btnEscalarJogador = document.getElementById("escalar-jogador");
+
+//inputs
+
 
 //a escalação já inicia com alguns nomes.
 let escalacao = [
@@ -72,8 +78,67 @@ function apagarTodos(){
     gerarTabelaEscalacao();
 }
 
+function alteraTituloTabela(){
+    const titleTable = document.getElementById("title-table");
+    const inputNomeTime = document.getElementById("input-nome-time");
 
+    //mandar validar este antes
+    titleTable.innerText = inputNomeTime.value;
+}
 
+function escalarJogador(){
+    const inputNomeJogador = document.getElementById("input-nome-jogador");
+    const posicao = document.getElementById("posicao");
+    const numCamisa = document.getElementById("camisa");
+    
+    let posicaoEscolhida = posicao.options[posicao.selectedIndex].text;
+
+    let jogador = new Jogador(numCamisa.value, inputNomeJogador.value, posicaoEscolhida);
+
+    /*console.log(numCamisa.value);
+    console.log(inputNomeJogador.value); //nome jogador
+    console.log(posicaoEscolhida); //posicao
+*/
+    escalacao.push(jogador);
+    console.log(escalacao[escalacao.length -1])
+    incluirApenasUmJogador();
+
+    console.table(escalacao);
+    //gerarTabelaEscalacao();
+}
+
+function incluirApenasUmJogador(){
+    const table = document.getElementById("table");
+    const tableTr = document.createElement("tr");
+    tableTr.classList.add("tr-control");
+    table.appendChild(tableTr);
+
+    const tdCamisa = document.createElement("td");
+    tdCamisa.innerText=escalacao[escalacao.length -1].camisa;
+    tableTr.appendChild(tdCamisa);
+
+    const tdNomeJogador = document.createElement("td");
+    tdNomeJogador.innerText=escalacao[escalacao.length -1].nome;
+    tableTr.appendChild(tdNomeJogador);
+
+    const tdPosicao = document.createElement("td");
+    tdPosicao.innerText=escalacao[escalacao.length -1].posicao;
+    tableTr.appendChild(tdPosicao);
+
+    const editarIcon = document.createElement("td");
+    const iTagEdit = document.createElement("i");
+    iTagEdit.classList.add("fa");
+    iTagEdit.classList.add("fa-pencil");
+    editarIcon.appendChild(iTagEdit);
+    tableTr.appendChild(editarIcon);
+
+    const deleteIcon = document.createElement("td");
+    const iTagDelete = document.createElement("i");
+    iTagDelete.classList.add("fa");
+    iTagDelete.classList.add("fa-trash");
+    deleteIcon.appendChild(iTagDelete);
+    tableTr.appendChild(deleteIcon);
+}
 
 /**
  * EVENTOS
@@ -84,4 +149,15 @@ window.onload = gerarTabelaEscalacao();
 btnApagarTodos.addEventListener("click",()=>{
     apagarTodos();
 });
+
+btnAlterarNomeTime.addEventListener("click",()=>{
+    alteraTituloTabela();
+});
+
+btnEscalarJogador.addEventListener("click",()=>{
+    escalarJogador();
+});
+
+
+
 
